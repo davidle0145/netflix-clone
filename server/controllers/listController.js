ad57e9b45e6a4477fef803891cd276cc7ab733d4
LE THANH DAT <ledat10b8@gmail.com>
2023-08-05 15:25:23 +0700

    crud List BE

--- server/controllers/listController.js
+++ server/controllers/listController.js
@@ -1,1 +1,113 @@
 import List from '../models/List.js'
+
+export const createList = async (req, res) => {
+    if (req.user.isAdmin) {
+        const newList = new List(req.body)
+        try {
+            const savedList = await newList.save()
+            res.status(201).json({
+                success: true,
+                message: "Successfully created new List",
+                data: savedList
+            })
+        } catch (err) {
+            res.status(500).json({
+                success: false,
+                message: err
+            })
+        }
+    } else {
+        res.status(403).json({
+            success: false,
+            message: "You are not allowed!"
+        })
+    } 
+}
+
+export const updateList = async (req, res) => {
+    if (req.user.isAdmin) {
+        try {
+            const updatedList = await List.findByIdAndUpdate(req.params.id, {
+                $set: req.body
+            }, {new:true})
+            res.status(201).json({
+                success: true,
+                message: "Successfully updated info List",
+                data: updatedList
+            })
+        } catch (err) {
+            res.status(500).json({
+                success: false,
+                message: err
+            })
+        }
+    } else {
+        res.status(403).json({
+            success: false,
+            message: "You are not allowed!"
+        })
+    } 
+}
+
+export const deleteList = async (req, res) => {
+    if (req.user.isAdmin) {
+        try {
+            await List.findByIdAndDelete(req.params.id)
+            res.status(201).json({
+                success: true,
+                message: "Successfully deleted List",
+            })
+        } catch (err) {
+            res.status(500).json({
+                success: false,
+                message: err
+            })
+        }
+    } else {
+        res.status(403).json({
+            success: false,
+            message: "You are not allowed!"
+        })
+    } 
+}
+
+export const getAllList = async (req, res) => {
+    const typeQuery = req.query.type
+    const genreQuery = req.query.genre
+    let list = []
+    try {
+        if (typeQuery) {
+            if (genreQuery) {
+                list = await List.aggregate([
+                    {$sample: {size:10}},
+                    {$match: {
+                        type: typeQuery,
+                        genre: genreQuery
+                    }}
+                ])
+            } else {
+                list = await List.aggregate([
+                    {$sample: {size:10}},
+                    {$match: {type: typeQuery}}
+                ])
+            }
+
+        } else {
+            list = await List.aggregate([
+                {$sample: {size:10}}
+            ])
+        }
+        res.status(201).json({
+            success: true,
+            count: list.length,
+            message: "Successfully get All List",
+            data: list
+        })
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err
+        })
+    }
+
+}
--- server/routes/lists.js
+++ server/routes/lists.js
@@ -1,6 +1,13 @@
 import express from 'express'
+import { verifyToken } from '../utils/verifyToken.js'
+import { createList, deleteList, getAllList, updateList } from '../controllers/listController.js'
 
 const router = express.Router()
+
+router.post("/create", verifyToken, createList)
+router.put("/:id", verifyToken, updateList)
+router.delete("/:id", verifyToken, deleteList)
+router.get("/", getAllList)
 
 
 export default router
